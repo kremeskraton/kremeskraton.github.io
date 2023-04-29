8f9f7a0de1e4da3a3243c6548b2a120bb8f5b1ab
kremeskraton <131336924+kremeskraton@users.noreply.github.com>
2023-04-29 21:03:11 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function toped() {
+    location.replace("https://www.tokopedia.com/kremeskraton")
+}
+
+function shopee() {
+    location.replace("https://shopee.co.id/kremeskraton")
+}
+
+function tiktokshop() {
+    location.replace("https://www.tiktok.com/@kremeskraton?lang=en")
+}
+
